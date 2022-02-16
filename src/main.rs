250a0b3b4efd1101a6a572393c02cd03d710302d
slegrand <ut5svsxgc@relay.firefox.com>
2022-02-15 22:06:28 -0500

    First testing

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::{process, path, env};
+
+
+fn main() {
+
+    // read the command line argument
+    let args: Vec<String> = env::args().collect();
+
+    // check that we have at least one argument (the filename)
+    let mut filename = "";
+    if args.len() > 1 {
+        filename = &args[1];
+    }
+
+    // check if file exists
+    if !std::path::Path::new(filename).exists() {
+        println!("Error: could not find the file [{}].", filename);
+        process::exit(1);
+    }
+
+    println!("filename = [{}]", filename);
+
+    // open the file
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        for c in line.expect("Nope").chars() {
+            println!("{}", c);
+        }
+    }
+
+
+    // let mut filename = "";
+    // if (args.len() > 0) {
+    //     filename = &args[1];
+    // }
+
+    // println!("{:?}", filename);
+    // let filename = "tmp/hello-world";
+
+    // let file = File::open(filename).unwrap();
+    // let reader = BufReader::new(file);
+
+    // for (index, line) in reader.lines().enumerate() {
+    //     let line = line.unwrap();
+
+    //     println!("{}. {}", index + 1, line);
+    // }
+}
