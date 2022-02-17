d92c89966057d945e6a3ec5dcc45be429ca23b3d
slegrand <ut5svsxgc@relay.firefox.com>
2022-02-17 07:52:55 -0500

    Add function to compute the jumps hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,106 @@
+use std::io::prelude::*;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
 use std::{process, path, env};
+use std::collections::HashMap;
 
+
+//----- constants
+const CODE_SIZE: usize = 32168;
+// const DATA_SIZE: usize = 32168;
+
+//----- functions
+// read the code from the disk
+fn read_code(filename: &str, code: &mut [u8]) -> Result<usize, String> {
+
+    // unable to find the file
+    if !path::Path::new(&filename).exists() {
+        return Err("Unable to find the file".to_string());
+    }
+
+    // read the code
+    let mut file = File::open(filename).unwrap();
+    let n = file.read(&mut code[..]).unwrap();
+
+    Ok(n)
+}
+
+// compute the hashmap for the jumps
+fn compute_jumps(code: &[u8], nbytes: usize) -> HashMap<usize, usize> {
+    let mut jumps: HashMap<usize, usize> = HashMap::new();
+    let mut stack: Vec<usize> = Vec::new();
+    let mut counter: usize = 0;
+
+    // parse the code and look for '[' and ']'
+    while counter < nbytes {
+        // retrieve the opcode
+        let opcode = code[counter as usize];
+
+        // push the current counter if we found '['
+        if opcode == 91 {
+            stack.push(counter)
+        }
+
+        // retrieve the last value if we found ']'
+        if opcode == 93 {
+            if stack.len() == 0 {
+                println!("Error: unbalanced number of '[' and ']' in the source code.");
+                process::exit(1);
+            }
+
+            // retrieve the position of the corresponding '['
+            let value = stack.pop().unwrap();
+
+            // insert the two value inside the hashmap
+            jumps.insert(value, counter + 1);           // '[' is map to the position after the ']'
+            jumps.insert(counter, value + 1);           // ']' is map to the position after the '['
+
+        }
+
+        // next opcode
+        counter += 1;
+    };
+
+    return jumps;
+}
+
+
+// run the code
+
+
+//----- variables
 
 fn main() {
+
+    let mut code: [u8; CODE_SIZE] = [0; CODE_SIZE];
+    // let mut data: [u8; DATA_SIZE] = [0; DATA_SIZE];
 
     // read the command line argument
     let args: Vec<String> = env::args().collect();
 
     // check that we have at least one argument (the filename)
-    let mut filename = "";
-    if args.len() > 1 {
-        filename = &args[1];
-    }
-
-    // check if file exists
-    if !path::Path::new(filename).exists() {
-        println!("Error: could not find the file [{}].", filename);
+    if args.len() == 0 {
+        println!("Please specify a source code file on the command line");
         process::exit(1);
     }
 
-    println!("filename = [{}]", filename);
-
-    // open the file
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
-
-    for line in reader.lines() {
-        for c in line.expect("Nope").chars() {
-            println!("{}", c);
+    // read the code
+    let nbytes = match read_code(&args[1], &mut code) {
+        Ok(n) => {
+            n
+        },
+        Err(e) => {
+            println!("{}", e);
+            process::exit(1);
         }
+    };
+
+    println!("{} bytes read.", nbytes);
+
+    // compute the jumps locations
+    let jumps = compute_jumps(&code, nbytes);
+
+    for (key, value) in jumps {
+        println!("jump[{}] = {}", key, value);
     }
-} 
+
+}
